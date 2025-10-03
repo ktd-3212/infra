72f376af010f4af453db07cd9676b5769943b2fa
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-03 17:16:22 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,35 @@
-terraform {
-  required_version = ">= 1.3.0"
+provider "aws" {
+  region = var.region
+}
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.20"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "eks-vpc"
+  cidr   = "10.0.0.0/16"
+
+  azs             = ["ap-southeast-1a", "ap-southeast-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 2
+      max_size       = 3
+      min_size       = 1
+      instance_types = ["t3.medium"]
     }
   }
-
+}

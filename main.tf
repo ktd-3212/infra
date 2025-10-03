3afd6282fb519cd102c36b0fe64bbc41218622d2
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-03 17:15:16 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,25 +12,3 @@ terraform {
     }
   }
 
-# Provider Kubernetes kết nối tới EKS cluster sẵn có
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-# Lấy thông tin EKS cluster đã có
-data "aws_eks_cluster" "cluster" {
-  name = "daidh-eks-cluster"
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = "daidh-eks-cluster"
-}
-
-# Resource test: tạo namespace trong EKS
-resource "kubernetes_namespace" "daidh-namespace" {
-  metadata {
-    name = "daidh-namespace"
-  }
-}

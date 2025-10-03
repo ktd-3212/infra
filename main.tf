b172e8cc9012892409eeffcb03c0eb24b7efbb08
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-03 17:13:16 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,20 +12,6 @@ terraform {
     }
   }
 
-  # Lưu state vào S3
-  backend "s3" {
-    bucket         = "terraform-daidh-state"     
-    key            = "eks-demo/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-lock"            
-    encrypt        = true
-  }
-}
-
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 # Provider Kubernetes kết nối tới EKS cluster sẵn có
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint

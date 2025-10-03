2379efe2a5868c345a6ab2ec9027308528c47f76
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-03 17:17:20 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.8.4"
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
+  cluster_version = "1.33"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets

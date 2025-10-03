0da48c1a4bfabc3edf98fcfeb9fb184fd31d06cb
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-03 16:57:32 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,15 +8,6 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "terraform-daidh-state"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = var.region
 }

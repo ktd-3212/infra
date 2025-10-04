66c9151e0d11b1ae9a63b37ab1c23eb1b42a4944
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-04 13:56:48 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,60 +13,60 @@ provider "aws" {
 }
 
 # 1. Tạo VPC
-resource "aws_vpc" "k21_vpc" {
+resource "aws_vpc" "daidh_vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "k21_vpc"
+    Name = "daidh_vpc"
   }
 }
 
 # 2. Tạo Internet Gateway (IGW)
-resource "aws_internet_gateway" "k21_igw" {
-  vpc_id = aws_vpc.k21_vpc.id
+resource "aws_internet_gateway" "daidh_igw" {
+  vpc_id = aws_vpc.daidh_vpc.id
   tags = {
-    Name = "k21_igw"
+    Name = "daidh_igw"
   }
 }
 
 # 3. Tạo Public Subnet 
-resource "aws_subnet" "k21_public_subnet" {
-  vpc_id                  = aws_vpc.k21_vpc.id
+resource "aws_subnet" "daidh_public_subnet" {
+  vpc_id                  = aws_vpc.daidh_vpc.id
   cidr_block              = var.public_subnet_cidr_block
   map_public_ip_on_launch = true # Tự động gán Public IP cho các EC2
   availability_zone       = var.aws_az
   tags = {
-    Name = "k21_public_subnet"
+    Name = "daidh_public_subnet"
   }
 }
 
 # 4. Tạo Route Table cho Public Subnet
-resource "aws_route_table" "k21_public_rt" {
-  vpc_id = aws_vpc.k21_vpc.id
+resource "aws_route_table" "daidh_public_rt" {
+  vpc_id = aws_vpc.daidh_vpc.id
   tags = {
-    Name = "k21_public_rt"
+    Name = "daidh_public_rt"
   }
 }
 
 # 5. Thêm Route Internet vào Public Route Table
-resource "aws_route" "k21_internet_route" {
-  route_table_id         = aws_route_table.k21_public_rt.id
+resource "aws_route" "daidh_internet_route" {
+  route_table_id         = aws_route_table.daidh_public_rt.id
   destination_cidr_block = "0.0.0.0/0" # Định tuyến tất cả lưu lượng ra Internet
-  gateway_id             = aws_internet_gateway.k21_igw.id
+  gateway_id             = aws_internet_gateway.daidh_igw.id
 }
 
-# 6. Liên kết Public Subnet với Public Route Table
-resource "aws_route_table_association" "k21_public_subnet_association" {
-  subnet_id      = aws_subnet.k21_public_subnet.id
-  route_table_id = aws_route_table.k21_public_rt.id
+# 6. Gắn Public Subnet với Public Route Table
+resource "aws_route_table_association" "daidh_public_subnet_association" {
+  subnet_id      = aws_subnet.daidh_public_subnet.id
+  route_table_id = aws_route_table.daidh_public_rt.id
 }
 
 # 7. Tạo Security Group cho EC2 Instance (Allow SSH và HTTP)
-resource "aws_security_group" "k21_sg" {
-  name        = "k21-web-sg"
+resource "aws_security_group" "daidh_sg" {
+  name        = "daidh-web-sg"
   description = "Allow HTTP and SSH inbound traffic"
-  vpc_id      = aws_vpc.k21_vpc.id
+  vpc_id      = aws_vpc.daidh_vpc.id
 
   # Ingress Rule: Allow HTTP (Port 80) từ mọi nơi
   ingress {
@@ -77,7 +77,7 @@ resource "aws_security_group" "k21_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Ingress Rule: Allow SSH (Port 22) từ mọi nơi (cần hạn chế trong môi trường Production)
+  # Ingress Rule: Allow SSH (Port 22) từ mọi nơi 
   ingress {
     description = "SSH from Internet"
     from_port   = 22
@@ -94,17 +94,17 @@ resource "aws_security_group" "k21_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "k21-sg"
+    Name = "daidh-sg"
   }
 }
 
 # 8. Tạo Instance EC2
-resource "aws_instance" "k21_webserver" {
+resource "aws_instance" "daidh_webserver" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.k21_public_subnet.id
+  subnet_id     = aws_subnet.daidh_public_subnet.id
   # Gán Security Group đã tạo
-  security_groups = [aws_security_group.k21_sg.id]
+  security_groups = [aws_security_group.daidh_sg.id]
   key_name        = var.key_name
   user_data       = <<-EOF
               #!/bin/bash
@@ -112,10 +112,10 @@ resource "aws_instance" "k21_webserver" {
               sudo yum install httpd -y
               sudo systemctl start httpd
               sudo systemctl enable httpd
-              echo "<h1>Hello from K21 Academy EC2!</h1>" > /var/www/html/index.html
+              echo "<h1>Hello from daidh devops 2503 EC2</h1>" > /var/www/html/index.html
               EOF
 
   tags = {
-    Name = "k21_webserver"
+    Name = "daidh_webserver"
   }
 }

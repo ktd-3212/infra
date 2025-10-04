85b4d41bb9ae86c59f9441d191d5fecf27dfdbb8
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-04 12:36:00 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,9 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
+output "website_url" {
+  description = "Địa chỉ IP công cộng của Webserver"
+  value       = "http://${aws_instance.k21_webserver.public_ip}"
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_kubeconfig" {
-  value = module.eks.kubeconfig
+output "vpc_id" {
+  description = "ID của VPC được tạo"
+  value       = aws_vpc.k21_vpc.id
 }

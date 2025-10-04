4a9883d7bd8ba71a5d7d763834a84af7cea4d8ff
ktd-3212 <87775826+ktd-3212@users.noreply.github.com>
2025-10-04 13:57:19 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "website_url" {
   description = "Địa chỉ IP công cộng của Webserver"
-  value       = "http://${aws_instance.k21_webserver.public_ip}"
+  value       = "http://${aws_instance.daidh_webserver.public_ip}"
 }
 
 output "vpc_id" {
   description = "ID của VPC được tạo"
-  value       = aws_vpc.k21_vpc.id
+  value       = aws_vpc.daidh_vpc.id
 }
